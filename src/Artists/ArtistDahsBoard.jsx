e081ac051a83d10d4dc4716419c79cf9b4da8340
Oksana Dotsenko <oksanadotsenko967@gmail.com>
2025-06-10 17:23:46 +0100

    new

--- src/Artists/ArtistDahsBoard.jsx
+++ src/Artists/ArtistDahsBoard.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import { UserAuthContext } from '../Forms/UserAuthContext';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faArrowUpFromBracket, faImage, faTrash} from '@fortawesome/free-solid-svg-icons';
 // import { useMap } from 'https://cdn.esm.sh/react-leaflet/hooks'
 
 function ArtistDashboard() {
@@ -11,11 +12,41 @@ function ArtistDashboard() {
 
   return (
     <div className='flex  items-center flex-col h-full justify-between'>
-      <section className='flex  w-[80vw] justify-between py-4  border-b border-ternary-dark'>
+      <section className='flex  w-[80vw] justify-between py-4 pt-16 border-b border-ternary-dark'>
        <div className=' '><h2 className='font-semibold text-primary-dark text-2xl'>Welcome, {user.firstName} {user.lastName}!</h2></div> 
       </section>
-<section>
-  <div></div>
+<section className='grid grid-cols-[30%_50%] w-[80vw] gap-16 py-10 justify-center'>
+  <div className='flex flex-col  border-b'>
+    <h2 className='pb-4 font-semibold'>Current art hosts:</h2>
+    <div className='flex justify-between text-xs py-2 px-4 bg-ternary-light'>
+      <p >Art Host1</p>
+      <p>Location </p>
+      </div>
+  </div>
+  <div className='flex flex-col'>
+    <h2 className='pb-4 font-semibold' >Next steps:</h2>
+      <p className='text-xs py-2' >Upload your art and receive your exhibition schedule on the spot:</p>
+    <div className='grid grid-cols-[50%_50%] gap-10 justify-between text-xs py-2 px-4 '>
+     <div className='border border-ternary-medium border-dashed rounded-md col-span-1 justify-center items-center align-middle text-center py-6 bg-ternary-light'>
+   <FontAwesomeIcon className='text-2xl self-center text-center py-4' icon={faArrowUpFromBracket} />
+   <p className='text-center py-2 bg-ternary-medium flex rounded-full w-auto self-center' >Browse Files</p>
+   <p className='text-center py-2 text-ternary-medium'>Max size: 200Mb</p>
+     </div>
+     
+     <div className='  col-2  border-b'>
+      <h2 className='font-semibold pb-4'>Uploaded files:</h2>
+        <div className='flex items-center justify-between px-4'>
+          <div className='flex items-center py-2 text-[0.7rem] gap-2'>
+            <FontAwesomeIcon className='text-ternary-medium' icon={faImage}/>
+            <p >Filename</p>
+
+          </div>
+          <FontAwesomeIcon className='text-red-600'icon={faTrash}/>
+        </div>
+     </div>
+
+      </div>
+  </div>
 </section>
 
     </div>
