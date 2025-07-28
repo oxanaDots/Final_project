8cde0c54c99eedf4deda4f27bb0de51e878c33ce
Oksana Dotsenko <oksanadotsenko967@gmail.com>
2025-07-28 18:24:07 +0100

    new

--- api/enterprises.cjs
+++ api/enterprises.cjs
@@ -1,13 +1,8 @@
 
-const express = require('express');
-const cors    = require('cors');
-const data    = require('./enterprises.json');
+import data from './enterprises.json';
 
-const app = express();
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.json(data);
-});
-
-module.exports = app;
+export function GET() {
+  return new Response(JSON.stringify(data), {
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
