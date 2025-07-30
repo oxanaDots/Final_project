d74befdc075325985780efd6ed75e7b5e3bd0a7d
Oksana Dotsenko <oksanadotsenko967@gmail.com>
2025-07-30 22:24:56 +0100

    new

--- api/enterprises.cjs
+++ api/enterprises.cjs
@@ -3,6 +3,14 @@ const app = express()
 const PORT = 3001
 const cors = require('cors');
 
+<<<<<<< HEAD
+=======
+const express = require('express');
+const cors    = require('cors');
+const data    = require('./enterprises.json');
+
+const app = express();
+>>>>>>> 1f91b11 (new)
 app.use(cors());
 
 const enterprises = require('./enterprises.json');
