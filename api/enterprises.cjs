3537f0c6d19a5b93b9a75707b09800abedf7f492
Oksana Dotsenko <oksanadotsenko967@gmail.com>
2025-08-04 16:28:23 +0100

    new

--- api/enterprises.cjs
+++ api/enterprises.cjs
@@ -1,22 +1,21 @@
-const express = require('express')
-const app = express()
-const PORT = 3001
-const cors = require('cors');
-app.use(cors());
+// const express = require('express')
+// const app = express()
+// const PORT = 3001
+// const cors = require('cors');
+// app.use(cors());
 
-const enterprises = require('./enterprises.json');
-app.get('/api/enterprises', (req, res)=>{
-    res.json(enterprises)
-})
+// const enterprises = require('./enterprises.json');
+// app.get('/api/enterprises', (req, res)=>{
+//     res.json(enterprises)
+// })
 
 
-app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+// app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
 
-module.exports = app; // api/enterprises.js
-// import data from './enterprises.json';
+// module.exports = app; 
 
-// export function GET() {
-//   return new Response(JSON.stringify(data), {
-//     headers: { 'Content-Type': 'application/json' }
-//   });
-// }
+import enterprises from '../../enterprises.json';
+
+export default function handler(req, res) {
+  res.status(200).json(enterprises);
+}
