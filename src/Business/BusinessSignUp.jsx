b267eac70b36d4736a7cb92e2f290ead1190c89a
Oksana Dotsenko <oksanadotsenko967@gmail.com>
2025-08-11 20:12:49 +0100

    new

--- src/Business/BusinessSignUp.jsx
+++ src/Business/BusinessSignUp.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import InputField from '../Components/InputField';
 import { useForm } from 'react-hook-form';
 import { useNavigate, Link } from 'react-router-dom';
 import services from '../services.json'
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, onAuthStateChanged, reload } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { auth, db } from "../firebase.js"; 
 import { fetchData } from '../utilities/fetchData.js';
@@ -14,15 +14,31 @@ function BusinessSignup() {
   const [signUpError, setSignUpError] = useState('')
   const [geoCodeValue, setGeoCode] = useState( '')
   const [submitted, setSubmitted] = useState(false)
+  const [verified, setVerified] = useState(false)
+  const [checking, setChecking] = useState(false)
+  const [verificationError, setVerificationError] = useState(false)
+  const [formData, setFormData] = useState(null)
+ console.log(formData)
+ console.log(geoCode)
  
+// track if email has been verified by user by calling onAuthStateChanged observer 
+  useEffect(() => {
+    const callToAuth = onAuthStateChanged(auth, async (user) => {
+      if (!user) return setVerified(false);
+      await reload(user);
+      setVerified(user.emailVerified === true);
+    });
+    return callToAuth;
+  }, []);
 
 
+
+  // submit user request and eail verification request
   async function onSubmit (data) {
-
   try {
-    const enterprisesData = await fetchData('https://final-project-red-delta.vercel.app/api/enterprises')
-    //  const enterprisesData = await fetchData('http://localhost:3001/api/enterprises')
-
+    // const enterprisesData = await fetchData('https://final-project-red-delta.vercel.app/api/enterprises')
+     const enterprisesData = await fetchData('http://localhost:3001/api/enterprises')
+     setFormData(data)
    
    const geoCodedLoc = await geoCode(`${data.location}, ${data.postcode}`)
    console.log(geoCodedLoc)
@@ -30,7 +46,7 @@ function BusinessSignup() {
    console.log(foundEnterprise)
      console.log(data.location, data.postcode)
 
-    !geoCodedLoc && setGeoCode( 'Wrong address')
+    !geoCodedLoc ? setGeoCode( 'Wrong address') : setGeoCode(geoCodedLoc)
       foundEnterprise.length === 0 &&  setSignUpError('No record of your company has been found. Try again.')
    
     if (geoCodedLoc && foundEnterprise.length > 0){
@@ -40,24 +56,15 @@ function BusinessSignup() {
       data.email,
       data.password
     );
-       const business = userCredential.user;
 
-       await setDoc(doc(db, "businesses", business.uid), {
-       businessName: data.businessName,
-       firstName: data.firstName,
-       lastName: data.lastName,
-       email: data.email,
-       location: data.location,
-       postcode: data.postcode,
-       phoneNumber: data.phoneNumber,
-       geoLocation:  geoCodedLoc,
-       business_type: data.business_type,
-       role: 'business',
-       createdAt: new Date()
-      });
-      console.log("Business user created and stored in Firestore");
+      await sendEmailVerification(userCredential.user)
+
+      
+  
       setSubmitted(true)
-    } 
+    } else{
+      return
+    }
   
   } catch (error) {
     console.error("Error during signup:", error.message);
@@ -66,13 +73,47 @@ function BusinessSignup() {
 };
 
 
+async function checkVerification(){
+  const user = auth.currentUser
+  setChecking(true)
+ if (!user) return
+ try{
+   await reload(user)
+   if(!user.emailVerified){
+    setVerificationError(false)
+   } else{
+     
+ setVerified(true)
+       await setDoc(doc(db, "businesses", user.uid), {
+       businessName: formData.businessName,
+       firstName: formData.firstName,
+       lastName: formData.lastName,
+       email: formData.email,
+       location: formData.location,
+       postcode: formData.postcode,
+       phoneNumber: formData.phoneNumber,
+       geoLocation:  geoCodeValue,
+       business_type: formData.business_type,
+       role: 'business',
+       createdAt: new Date()
+      });
+      console.log("Business user created and stored in Firestore");
+   }
+
+ } catch(err){
+  console.error(err)
+ } finally{
+  setChecking(false)
+ }
+}
   
   return (
     <div className=" flex flex-col  p-4 justify-center text-center items-center">
 
 
  <div className=' flex  w-[40rem] justify-center items-center'>
-      {!submitted ? <form 
+      {!submitted ? (
+        <form 
         onAnimationStart={e => {
       if (e.animationName === 'onAutoFillStart') {
         const input = e.target;
@@ -84,7 +125,7 @@ function BusinessSignup() {
             <legend className="text-xl text-center font-semibold mb-4">Create an Account</legend>
                 { signUpError.length > 0 && 
               
-                <p data-testid="signup-error"  className='text-red-500 flex mt-4 justify-center rounded-md text-xs bg-red-50 border-red-400 self  text-center border py-4'>{signUpError}</p>
+                <p data-testid="signup-error"  className='text-red-500 flex mt-4 mb-2 justify-center rounded-md text-xs bg-red-50 border-red-400 self  text-center border py-4'>{signUpError}</p>
               
                 }
                 {geoCodeValue === 'Wrong address' && <p className='text-red-500 flex mb-4 mt-2 justify-center rounded-md text-xs bg-red-50 border-red-400 self  text-center border py-4'>Make sure you entered correct location.</p>}
@@ -227,14 +268,19 @@ function BusinessSignup() {
     }}
     error={errors.confirmPassword}
   />
-  
 
-
-
-  
       <button type='submit'  data-testid="submit" className='submit-btn'>{isSubmitting? "Checking your company details":'Submit'}</button>
-    </form>:
-      <div className='flex align-middle pt-20 '>
+    </form>):
+    <>
+        {!verified && <div className='py-20'> 
+      <p className='p-6'>We sent you an email verification link to your email address. Click on the button below once verification has been completed.</p>
+     <button className='py-3 bg-primary-dark rounded-md px-4 text-secondary-light' onClick={checkVerification} disabled={checking}>
+    { checking && !verified ? "Checking your request" : "I have completed verififcation"}
+    </button>
+       </div>}
+      {!verificationError && verified &&
+      
+        <div className='flex align-middle pt-20 '>
         <div className='p-10 border  border-primary-dark rounded-md'>  
       <h2 className='text-green-600 font-semibold text-2xl py-6'>Account created successfully!</h2>
       <p className='text-xs'>Email confirmation has been sent to your email address with the next steps.</p>
@@ -242,8 +288,12 @@ function BusinessSignup() {
         <button  className='main-btn'>Return Home</button>
        </Link>
         </div>
-  </div>
-    }
+  </div>}
+  
+ { verificationError && <p>Email address could not be verified</p>}
+
+  
+  </>}
   </div>
 
 
